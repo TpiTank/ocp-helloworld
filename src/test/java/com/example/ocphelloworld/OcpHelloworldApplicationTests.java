3d5661c1fee03a43557ee1f53c2faba9c78ec164
tank.chen <tank.chen@tpisoftware.com>
2023-09-05 18:23:33 +0800

    project init .

---
+++ src/test/java/com/example/ocphelloworld/OcpHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocphelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
